269d08641d90e6b266712b6eb323b583fb3276e0
Kevin Day <kd@kevindayprogramming.com>
2021-08-12 15:20:49 +1000

    Add project files.

---
+++ ReactivePropertyGenerator/ReactivePropGenerator.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Code taken and adapted from the roslyn-sdk sample for Auto-Notify which is a source generator for
+ * properties of INotifyPropertyChanged objects.
+ * 
+ * https://github.com/dotnet/roslyn-sdk/blob/main/samples/CSharp/SourceGenerators/SourceGeneratorSamples/AutoNotifyGenerator.cs
+ * 
+ * I wanted much the same thing but for ReactiveUI 
+ */
+
+namespace ReactivePropertyGenerator
+{
+    [Generator]
+    public class ReactivePropGenerator : ISourceGenerator
+    {
+        private const string _attributeText = @"
+using System;
+namespace ReactiveUI
+{
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    [System.Diagnostics.Conditional(""ReactivePropGenerator_DEBUG"")]
+    sealed class ReactivePropAttribute : Attribute
+    {
+        public ReactivePropAttribute()
+        {
+        }
+        public string PropertyName { get; set; }
+    }
+}
+";
+
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // Register the attribute source
+            context.RegisterForPostInitialization((i) => i.AddSource("ReactivePropAttribute", _attributeText));
+
+            // Register a syntax receiver that will be created for each generation pass
+            context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("MicrosoftCodeAnalysisCorrectness", "RS1024:Compare symbols correctly", Justification = "<Pending>")]
+        public void Execute(GeneratorExecutionContext context)
+        {
+            // retrieve the populated receiver 
+            if (!(context.SyntaxContextReceiver is SyntaxReceiver receiver))
+                return;
+
+            // get the added attribute, and ReactiveObject
+            INamedTypeSymbol attributeSymbol = context.Compilation.GetTypeByMetadataName("ReactiveUI.ReactivePropAttribute");
+            INamedTypeSymbol reactiveObjectSymbol = context.Compilation.GetTypeByMetadataName("ReactiveUI.ReactiveObject");
+
+            // group the fields by class, and generate the source
+            foreach (IGrouping<INamedTypeSymbol, IFieldSymbol> group in receiver.Fields.GroupBy(f => f.ContainingType))
+            {
+                string classSource = ProcessClass(group.Key, group.ToList(), attributeSymbol, reactiveObjectSymbol, context);
+                context.AddSource($"{group.Key.Name}_ReactiveProp.cs", SourceText.From(classSource, Encoding.UTF8));
+            }
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
+        private string ProcessClass(INamedTypeSymbol classSymbol, List<IFieldSymbol> fields, ISymbol attributeSymbol, ISymbol reactiveObjectSymbol, GeneratorExecutionContext context)
+        {
+            if (!classSymbol.ContainingSymbol.Equals(classSymbol.ContainingNamespace, SymbolEqualityComparer.Default))
+            {
+                return null; //TODO: issue a diagnostic that it must be top level
+            }
+
+            string namespaceName = classSymbol.ContainingNamespace.ToDisplayString();
+
+            // begin building the generated source
+            StringBuilder source = new StringBuilder($@"
+namespace {namespaceName}
+{{
+    public partial class {classSymbol.Name} : {reactiveObjectSymbol.ToDisplayString()}
+    {{
+");
+
+            // create properties for each field 
+            foreach (IFieldSymbol fieldSymbol in fields)
+            {
+                ProcessField(source, fieldSymbol, attributeSymbol);
+            }
+
+            source.Append("} }");
+            return source.ToString();
+        }
+
+        private void ProcessField(StringBuilder source, IFieldSymbol fieldSymbol, ISymbol attributeSymbol)
+        {
+            // get the name and type of the field
+            string fieldName = fieldSymbol.Name;
+            ITypeSymbol fieldType = fieldSymbol.Type;
+
+            // get the ReactiveProp attribute from the field, and any associated data
+            AttributeData attributeData = fieldSymbol.GetAttributes().Single(ad => ad.AttributeClass.Equals(attributeSymbol, SymbolEqualityComparer.Default));
+            TypedConstant overridenNameOpt = attributeData.NamedArguments.SingleOrDefault(kvp => kvp.Key == "PropertyName").Value;
+
+            string propertyName = chooseName(fieldName, overridenNameOpt);
+            if (propertyName.Length == 0 || propertyName == fieldName)
+            {
+                //TODO: issue a diagnostic that we can't process this field
+                return;
+            }
+
+            source.Append($@"public {fieldType} {propertyName} {{ get => this.{fieldName}; set => this.RaiseAndSetIfChanged(ref this.{fieldName}, value); }}");
+
+            string chooseName(string fldName, TypedConstant explicitPropName)
+            {
+                if (!explicitPropName.IsNull)
+                {
+                    return explicitPropName.Value.ToString();
+                }
+
+                fldName = fldName.TrimStart('_');
+                if (fldName.Length == 0)
+                    return string.Empty;
+
+                if (fldName.Length == 1)
+                    return fldName.ToUpper();
+
+                return fldName.Substring(0, 1).ToUpper() + fldName.Substring(1);
+            }
+
+        }
+
+        /// <summary>
+        /// Created on demand before each generation pass
+        /// </summary>
+        class SyntaxReceiver : ISyntaxContextReceiver
+        {
+            public List<IFieldSymbol> Fields { get; } = new List<IFieldSymbol>();
+
+            /// <summary>
+            /// Called for every syntax node in the compilation, we can inspect the nodes and save any information useful for generation
+            /// </summary>
+            public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
+            {
+                // any field with at least one attribute is a candidate for property generation
+                if (context.Node is FieldDeclarationSyntax fieldDeclarationSyntax
+                    && fieldDeclarationSyntax.AttributeLists.Count > 0)
+                {
+                    foreach (VariableDeclaratorSyntax variable in fieldDeclarationSyntax.Declaration.Variables)
+                    {
+                        // Get the symbol being declared by the field, and keep it if its annotated
+                        IFieldSymbol fieldSymbol = context.SemanticModel.GetDeclaredSymbol(variable) as IFieldSymbol;
+                        if (fieldSymbol.GetAttributes().Any(ad => ad.AttributeClass.ToDisplayString() == "ReactiveUI.ReactivePropAttribute"))
+                        {
+                            Fields.Add(fieldSymbol);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
