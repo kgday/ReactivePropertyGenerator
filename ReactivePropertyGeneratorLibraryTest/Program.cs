76ccacae4fd68beb62f0b62a48bb202433b068b3
Kevin Day <kd@kevindayprogramming.com>
2021-08-13 15:29:46 +1000

    A commit with attempt to make it work but no joy as yet.

---
+++ ReactivePropertyGeneratorLibraryTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReactivePropertyGeneratorLibraryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ReactivePropertyGeneratorTestModels/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using ReactiveUI;
+
+namespace ReactivePropertyGeneratorTest.Models
+{
+    public partial class Person : ReactiveObject
+    {
+    }
+}
